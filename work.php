be0437e7e45ff54ea160c2b090b618ad57e17dcd
xiyobidasturchi0011@mail.com <xiyobidasturchi0011@gmail.com>
2024-10-30 13:48:48 +0500

    1.1.0

--- work.php
+++ work.php
@@ -7,20 +7,28 @@
     <link rel="stylesheet" href="navbarbill.css">
 </head>
 <body>
-        <form method = "GET">
-            <input type="datetime-local" name="Arrived_person" required><br><br>
-            <input type="datetime-local" name="leaved_person" required><br><br>
-            <button type="submit">Bill</button>
-        </form>
-
-
+    <form method="GET">
+        <label>Kelgan vaqti:</label>
+        <input type="datetime-local" name="Arrived_person" required><br><br>
+        <label>Ketgan vaqti:</label>
+        <input type="datetime-local" name="leaved_person" required><br><br>
+        <button type="submit">Hisoblash</button>
+    </form>
 </body>
-</html> 
+</html>
+
 <?php   
-        $difference = $leaved_person->diff($Arrived_person);  
-        echo "Kun: " . $difference->days . "<br>" . "<br>";
-        echo "Soat: " . $difference->i . "<br>" . "<br>";
-        echo "Daqiqa: " . $difference->m;
-           
-           
+if (isset($_GET['Arrived_person']) && isset($_GET['leaved_person'])) {
+    // Formadagi qiymatlarni DateTime obyekti qilish
+    $Arrived_person = new DateTime($_GET['Arrived_person']);
+    $leaved_person = new DateTime($_GET['leaved_person']);
+    
+    // Farqni hisoblash
+    $difference = $leaved_person->diff($Arrived_person);
+
+    // Farqni chiqarish
+    echo "Kun: " . $difference->days . "<br><br>";
+    echo "Soat: " . $difference->h . "<br><br>";
+    echo "Daqiqa: " . $difference->i;
+}
 ?>
